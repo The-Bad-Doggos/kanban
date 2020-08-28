8288fa4bc2c8e03d508166d2419ef80f2db065da
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-08-28 18:25:31 -0400

    added psuedo code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The Bad Doggos Were Here</h1>
-    </div>
-  );
+import Something from "./Something"
+
+class App extends Component {
+	constructor() {
+		this.state = {
+			// In state, we want an obj that will contain the TITLE, DESCRIPTION, PRIORITY, CURRENT STATE (To Do, Doing, Done) for each task
+
+			tasks: []
+		};
+	}
+	
+	render() {
+		return (
+			 <div className="App">
+				{/* Show a landing page to give the user a brief overview of site */}
+
+				{/* Show an empty kanban board with 3 columns, and a + to add more */}
+
+				{/* each column should have a "+" to add cards */}
+
+				{/* Maybe?? A modal pop up for the user to fill out the details of tasks (title, description, priority); ok/submit button */}
+
+				{/* .onSubmit Update this.setState with task */}
+
+				{/* Create a card template */}
+
+				{/* pass the this.state.task to <Card /> to render in the appropriate column */}
+
+				{/* "X" button on the card to delete card; .onDelete remove from this.state.task */}
+
+				{/* Click and drag to move cards between columns */}
+
+				{/* Click and drag to move cards within columns */}
+
+				{/* Delete a whole column -- Error handling: If col.length > 0 Error popup "please select where to move cards before deleting?" */}
+
+				{/* Edit the title/description/priority of a card */}
+
+				<Something />
+				<h1>The Bad Doggos Were Here</h1>
+			 </div>
+		);
+
+	}
 }
 
 export default App;
---
+++ src/Something.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Something = () => {
+    return(
+        <div>
+            <p>Something</p>
+        </div>
+    )
+}
+
+export default Something;
