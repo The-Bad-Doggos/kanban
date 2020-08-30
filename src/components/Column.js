ca89f179bbc128c348ad01182d3c6318dfbeeee5
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-08-30 15:02:01 -0400

    added a styles folder, rearranged Navbar and Landing on homescreen

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,14 +1,14 @@
 import React from "react";
 import AddTask from "./AddTask";
 
-const Column = () => {
+const Column = ({ name }) => {
   return (
     <div>
-      <h3>Column Name</h3>
-      <AddTask />
+      <h3>{name}</h3>
       <div>
         <p>Task will go here</p>
       </div>
+      <AddTask />
     </div>
   );
 };
